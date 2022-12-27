6b381d5d513839a3157a62c7c0a8f516e21f2cfa
Aryan <agarwalaryan139@gmail.com>
2022-12-28 00:07:27 +0530

    domain added

--- threeD/settings.py
+++ threeD/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-g+0xwb1rpnufc==*#(v_=ai7ye+zbkywgb#wn7f&stmefpdky=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
